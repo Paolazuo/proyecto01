92bc3a1e9fe5db3f7ecfad9b7524ebc2b0612756
Paolazuo <118744688+Paolazuo@users.noreply.github.com>
2022-11-23 17:58:56 -0500

    Delete funciones-01.js

--- js/funciones-01.js
+++
@@ -1,6 +0,0 @@
-function CalcularPromedio(){
-    var v1=document.getElementById("n1").value;
-    var v2=document.getElementById("n2").value;
-    var v3=document.getElementById("n3").value;
-    var pro= (parseFloat(v1)+parseFloat(v2)+parseFloat(v3))/3;document.getElementById("promedio").innerHTML=pro;
-}
